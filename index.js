9f7f6122858de45abeb9174a5ad9aec7d63da0b3
dedisemara <semaradedi@gmail.com>
2026-03-08 09:01:05 +0800

    w2_s2

--- index.js
+++ index.js
@@ -1,24 +1,76 @@
-//------------------------------------- compile time error--------------------------------------
-// error when the code can't compile to binary (device didn't translate code)
+// // ----------------------------------------DATA TYPE----------------------------------------
+// // javascript has 8 data types: number, string, boolean, undifine, null, symbol, object
+// // check typeof data
+// let address;
+// let age = 20;
+// let name = "Dedi Semara"
+// let isGraduated = true;
+// let middleName = null
 
-// Example V.1 because string did't have stop initiation ("). compiler will read all process string without stop 
-// console.log("Hello World!);
+// console.log(typeof address);
+// console.log(typeof age);
+// console.log(typeof name);
+// console.log(typeof isGraduated);
+// // typeof null === "object" is a bug, but fixing it would break a lot of code on the global web === this bug come from creator javascript
+// console.log(typeof middleName); 
+// // type conversion -> typeof typeof boolean to string
+// console.log(typeof typeof true);
 
-// Example V.2 because variable name === reserved keyword (exp: let, switch, for, if, do) 
-// let let = 2 + 5;
-// console.log(let);
+// // -----------Type Conversion Manual-------------
+// // if value save as string, it use more memory because it read "2" and "0" 
+// // but if it read as number it will read only 20
+// // number to string
+// let age = 20;
+// let converted = String(age);
+// console.log(typeof converted);
+// // string to number
+// let str = "123";
+// let num = Number(str);
+// console.log(typeof num);
+// // boolean true
+// console.log(Boolean(1));
+// console.log(Boolean("false"));
+// console.log(Boolean(" "));
+// // boolean false
+// console.log(Boolean(0));
+// console.log(Boolean(""));
+// console.log(Boolean(null));
 
+// // -----------Type Automatic Conversion === type coercion-------------
+// // it will run because bug of system javascript === it will convert string to number automatic
+// // (* or /) it will run matematic operation
+// let result = "8" / "2";
+// console.log(result);
+// console.log(typeof result);
+// // example (+). JavaScript provides several methods for string concatenation, the process of joining two or more strings into a single string. The most common methods are using the + operator
+// let age = 20;
+// let text = age + " years old";
+// console.log(text);
+// console.log(typeof text);
+// // hexadecimal
+// let number = 0x7D;
+// console.log(number);
+// // hexadecimal from sting
+// let str = '7D';
+// const num = Number.parseInt(str, 16);
+// console.log(num);
+// let str1 = '10';
+// const num1 = Number.parseInt(str1, 16);
+// console.log(num1);
+// const num2 = Number.parseInt(str1);
+// console.log(num2);
 
-//------------------------------------- run time error--------------------------------------
-// error when the code can compile to binary (device translate code) but when the binary processed it show error
-// Example 1 -> dedi semara printed but code after that will stop running because error
-// console.log('Dedi Semara');
-// let age = null;
-// console.log(age.name);
-
-// Example 2 -> dedi semara did't printed because from first line code have issue when processing data
-let age = null;
-console.log(age.name);
-console.log('Dedi Semara');
-
-
+// ----------- Equality Operator-------------
+// in database endcoding "A" == 65 and "a" == 97
+console.log(Boolean("a" == "A"));
+// unique case in javascript because type coersion in "=="
+console.log(Boolean("10" == 10));
+// if you want to stike role use two step process "==="
+// 1. are the data type the same 2. do === process
+// example false
+console.log(Boolean("10" === 10));
+// example true
+console.log(Boolean(10 === 10));
+// example false because not equality
+console.log(Boolean(5 !==  5));
+// note: always use (=== or !==)
