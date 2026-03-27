1aa73eec89b05fb6dc71f5ea25bd538b792e44ac
dedi semara <semaradedi@icloud.com>
2026-03-27 16:23:26 +0800

    w2_s4

--- index.js
+++ index.js
@@ -1,63 +1,121 @@
-// -------------------------------MORE ABOUT OBJECT #2----------------------------------------
+// -------------------------------MORE ABOUT OBJECT #3----------------------------------------
 // example 1
-let price = 100;
-let copyprice = price;
-copyprice = 200;
-console.log(copyprice); // value 200
-console.log(price); // value 100
+const rectangle = {
+    width: 5,
+};
+rectangle.height = 3;
+console.log(rectangle);
 
+// object with function it call method
 // example 2
-let rectangle = { width: 5, height: 3, };
-let copyrectangle = rectangle;
-copyrectangle.width = 15;
-console.log(copyrectangle.width); // value {width: 15}
-console.log(rectangle.width); // value {width: 15}
-
-// object copy by reference but if not object will copy by value
-
-// now lets doing the right copy
-let rectangle1 = { width: 5, height: 3, };
-let copyrectangle1 = {
-    width: rectangle1.width,
-    height: rectangle1.height
+const person = {
+    name: "dedi",
+    age: 27,
+};
+function sayHello() {
+    console.log("Hello!");
 }
-copyrectangle1.width = 15;
-console.log('Right value copy by reference: ')
-console.log(copyrectangle1.width); // value 15
-console.log(rectangle1.width); // value 5
+person.sayHello = sayHello;
+person.sayHello();
+console.log(person);
 
-// or use object assign to sparate referenece
-let rectangle2 = { width: 5, height: 3, };
-let copyrectangle2 = Object.assign({}, rectangle2);
-copyrectangle2.width = 15;
-console.log('Right value copy by reference use assign: ')
-console.log(copyrectangle2.width); // value 15
-console.log(rectangle2.width); // value 5   
+// you can also use function expression
+// make it simple without declare function first
+// example 3
+const person1 = {
+    name: "semara",
+    age: 27,
+};
+person1.sayHello= function() {
+    console.log("Hello!");
+};
+person1.sayHello();
 
-// -------------------------------ternary operator----------------------------------------
-const temperature = 38;
+// example 3 properties with 2 method
+const person2 = { name: "dedi" };
+person2.sayHello = function() {
+    console.log("Hello!");
+};
+person2.jump = function() {
+    console.log("Jump");
+};
+console.log(person2);
 
-const result = temperature >= 37 ? 'sick' : 'healthy';
-console.log(result); // value is 'sick'
+// ownership in javascript
+const rectangle1 = {
+    width: 10,
+    height: 20,
+};
+function getRectangleArea(r) {
+    return r.width * r.height;
+};
+console.log(getRectangleArea(rectangle1));
+// but you can use this
+rectangle1.getArea = function() {
+    return this.width * this.height;
+};
+console.log(rectangle1.getArea());
 
-// this code call ninja code. NOT RECOMENDED FOR PROFESSIONAL PROGRAMMERS
-// example
-const age = 1; 
-// <= 5 == 'balita'
-// 5 - 17 == 'anak-anak'
-// 18 == 'dewasa'
-const result1 = age < 18 ? age < 5 ? 'balita' : 'anak-anak' : 'dewasa';
-console.log(result1); // value is 'balita'
+// but we can input function in object
+const rectangle2 = {
+    width: 20,
+    height: 20,
+    getArea: function(){
+        return this.width * this.height;
+    },
+};
+console.log(rectangle2.getArea());
 
-// you should select this method for professional
-let category;
-if (age <  5){
-    category = 'balita';
-} else if (age < 18){
-    category = 'anak-anak';
-} else {
-    category = 'dewasa';
-}
+// combine all
+const circle = {
+    radius: 100
+};
+circle.phi = 3.14;
+circle.getArea = function(){
+    return this.radius * this.phi;
+};
+console.log(circle.getArea());
 
-console.log(category); // value is 'dewasa'
+// arrow function doesn't have "this"
+circle.getArea1 = () => {
+    return this.radius * this.phi; // "this" will be undifined
+};
+console.log(circle.getArea1());
 
+// constructor function
+// const rectangle3 = {
+//     width: 5,
+//     height: 34,
+//     getArea: function(){
+//         return this.width * this.height;
+//     },
+// };
+// const rectangle4 = {
+//     width: 12,
+//     height: 20,
+//     getArea: function(){
+//         return this.width * this.height;
+//     },
+// };
+// const rectangle5 = {
+//     width: 2,
+//     height: 30,
+//     getArea: function(){
+//         return this.width * this.height;
+//     },
+// };
+// we can simplyfy this code by construcor function
+function Rectangle( width, height ) {
+    this.width = width;
+    this.height = height;
+    this.getArea = function() {
+        return this.width * this.height;
+    };
+};
+const rectangle3 = new Rectangle(5, 34);
+const rectangle4 = new Rectangle(12, 20);
+const rectangle5 = new Rectangle(2, 30);
+
+console.log(rectangle3.getArea());
+console.log(rectangle4.getArea());
+console.log(rectangle5.getArea());
